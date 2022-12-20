120fe8a601c8fa77776a4e27572837d619b8ab80
Mohammed_Adil <adildxi10@gmail.com>
2022-12-21 00:33:45 +0530

    Add files via upload

--- Navbar.js
+++ Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
 <div className="container-fluid">
 <b><a className="navbar-brand" href="/">{props.title} </a></b>
 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,8 +14,16 @@ export default function Navbar(props) {
     <a className="nav-link active" aria-current="page" href="/">Home</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="/">Theme</a>
+    <a className="nav-link" href="/">About Us</a>
   </li>
+<li>
+  <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} my-2 mx-2`}>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable dark mode</label>
+  <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+</div>
+</li>
+
+{/*   
   <li className="nav-item dropdown">
     <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
       Dropdown
@@ -26,11 +34,13 @@ export default function Navbar(props) {
       <li><hr className="dropdown-divider"/></li>
       <li><a className="dropdown-item" href="/">Something else here</a></li>
     </ul>
-  </li>
+  </li> */}
   <li className="nav-item">
 
   </li>
 </ul>
+
+
 <form className="d-flex" role="search">
   <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
   <button className="btn btn-success" type="submit">Search</button>
